8db7cf0a4037a286a2daf1738e2536aef5c5394f
Jonas Rocha Castanheira <jonas.r.castanheira@accenture.com>
2020-06-22 00:49:44 -0300

    New satellite details page; New functionalities for the static main page service worker; Dynamic caching for the satellite details pages.

---
+++ js/details.js
@@ -0,0 +1,28 @@
+let xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", "data/data.json", true);
+
+xhttp.send();
+
+xhttp.onreadystatechange = function() {
+  if(this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(this.response);
+    let gridHtml = '';
+    data.forEach((element) => {
+      gridHtml+= fillGridItemTemplate(element);
+    });
+
+    let satGrid = document.getElementById("sattelite_grid");
+    satGrid.insertAdjacentHTML("beforeend", gridHtml);
+  }
+};
+
+function fillGridItemTemplate(element) {
+  return `<a class="grid-item" href="/details.html?id=${element.id}">
+      <figure class="grid-item--figure">
+        <img class="grid-item--image" src="${element.thumbnail}" alt="${element.name}">
+      </figure>
+      <h3 class="grid-item--info">${element.name}</h3>
+    </a>`;
+}
+
---
+++ js/ui.js
@@ -0,0 +1,33 @@
+let xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", "data/data.json", true);
+
+xhttp.send();
+
+xhttp.onreadystatechange = function() {
+  if(this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(this.response);
+    let gridHtml = '';
+    data.forEach((element) => {
+      gridHtml+= fillGridItemTemplate(element);
+    });
+
+    let satGrid = document.getElementById("sattelite_grid");
+    satGrid.insertAdjacentHTML("beforeend", gridHtml);
+  }
+};
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log("Entered beforeinstallprompt event!");
+  deferredInstallPrompt = event;
+});
+
+function fillGridItemTemplate(element) {
+  return `<a class="grid-item" href="/${element.id}">
+      <figure class="grid-item--figure">
+        <img class="grid-item--image" src="${element.thumbnail}" alt="${element.name}">
+      </figure>
+      <h3 class="grid-item--info">${element.name}</h3>
+    </a>`;
+}
+
